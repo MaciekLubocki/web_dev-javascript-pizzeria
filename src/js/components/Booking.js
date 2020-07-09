0ea6ef61cdfb1effd7e8a1560bca53c4b5ca4799
Maciek Lubocki <mail2maciek@gmail.com>
2020-07-09 22:28:52 +0200

    booking update

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -4,7 +4,6 @@ import AmountWidget from './AmountWidget.js';
 import { DatePicker } from './DatePicker.js';
 import { HourPicker } from './HourPicker.js';
 
-
 export class Booking {
   constructor(element) {
     const thisBooking = this;
